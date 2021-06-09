a9a10b3384b1a35da99ff63f5a719a4f71b0b404
Hugo da Silva Vitor <hugodasilvav0209@gmail.com>
2021-06-09 18:58:48 -0300

    Testada e funcionante

--- Classes/Usuario.cs
+++ Classes/Usuario.cs
@@ -10,29 +10,29 @@ namespace ProjetoProdutos.Classes
         private string Nome { get; set; }
         private string Senha { get; set; }
         private DateTime DataCadastro { get; set; }
-        //List<Usuario> listaUsuarios = new List<Usuario>();
+        List<Usuario> listaUsuarios = new List<Usuario>();
 
-        //public Usuario()
-        //{}
-        //public Usuario(string _email, string _nome, string _senha)
-        //{   
-            //this.Codigo = 1;
-            //this.Email = _email;
-            //this.Senha = _senha;
-            //this.Nome = _nome;
-           // this.DataCadastro = DateTime.Now;
-        //}
+        public Usuario()
+        {}
+        public Usuario(string _email, string _nome, string _senha)
+        {   
+            this.Codigo = 1;
+            this.Email = _email;
+            this.Senha = _senha;
+            this.Nome = _nome;
+            this.DataCadastro = DateTime.Now;
+        }
 
-        //public string Cadastrar(Usuario usuario)
-        //{
-           // listaUsuarios.Add(usuario);
-        //    return "Usuario cadastrado com sucesso!";
-        //}
+        public string Cadastrar(Usuario usuario)
+        {
+           listaUsuarios.Add(usuario);
+           return "Usuario cadastrado com sucesso!";
+        }
 
-        //public string Deletar(Usuario usuario)
-       // {
-            //listaUsuarios.Remove(usuario);
-            //return ("Usuario deletado com sucesso!");
-        //}
+        public string Deletar(Usuario usuario)
+        {
+            listaUsuarios.Remove(usuario);
+            return ("Usuario deletado com sucesso!");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ProjetoProdutos.Classes;
 
 namespace ProjetoProdutos
 {
